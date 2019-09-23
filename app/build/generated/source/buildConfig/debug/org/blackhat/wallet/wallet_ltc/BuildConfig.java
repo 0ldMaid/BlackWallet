78064cf2e8b7c0ef13b8ea89b6a93977b390984c
0ldMaid <0ldMaid@users.noreply.github.com>
2019-09-23 19:23:27 +0700

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/org/blackhat/wallet/wallet_ltc/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.blackhat.wallet.wallet_ltc;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.blackhat.wallet.wallet_ltc";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 164;
+  public static final String VERSION_NAME = "3.30.10";
+}
